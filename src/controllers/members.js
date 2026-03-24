e9857d18c147abcfec162860151e76a3c850eb58
yonatanfeseha <122829231+yonatanfeseha@users.noreply.github.com>
2026-03-24 20:05:02 +0300

    controller updated

--- src/controllers/members.js
+++ src/controllers/members.js
@@ -5,14 +5,16 @@ export const getMembers = async (req, res) => {
     const page = parseInt(req.query.page) || 1;
     const limit = parseInt(req.query.limit) || 10;
 
-    const members = await getAllMembers(page, limit);
+    const result = await getAllMembers(page, limit);
 
     res.json({
-      page,
-      limit,
-      data: members,
+      success: true,
+      ...result,
     });
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
   }
 };
