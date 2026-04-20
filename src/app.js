ab1ab21a616f6ba0b6f9bc1555bdce9ac6e28986
dawit-808 <tesfayedawit22090582@gmail.com>
2026-04-20 16:46:49 +0300

    cors

--- src/app.js
+++ src/app.js
@@ -22,7 +22,12 @@ const app = express();
 
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:8080",
+    credentials: true,
+  }),
+);
 app.use(morgan("dev"));
 app.use(cookieParser());
 
